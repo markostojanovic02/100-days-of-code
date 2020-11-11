c8a16d33329042f0321048d12174e8ff88ba42c0
Marko Stojanovic <markostojanovic482@gmail.com>
2020-11-11 20:07:59 +0100

    Added solution for #28

---
+++ 28/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    double N;
+    std::cin >> N;
+
+    double r;
+    r = sqrt(N);
+    printf("%.2f\n", r);
+
+    return 0;
+}
